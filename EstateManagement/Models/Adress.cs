0d60b9eaeb14e815b63040b7b62b0ae264b9fb87
zbikowskiL <lx.zbikowski@gmail.com>
2017-12-11 21:20:24 +0100

    fisrt commit

---
+++ EstateManagement/Models/Adress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstateManagement.Models
+{
+    public class Adress
+    {
+        public int AdressId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ EstateManagement/Models/Owner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstateManagement.Models
+{
+    public class Owner
+    {
+        public int OwnerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
