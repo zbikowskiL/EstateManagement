0c0aad5d981c561049d65b8b7641a47decfa7651
zbikowskiL <lx.zbikowski@gmail.com>
2018-02-16 23:29:50 +0100

    Add first PropertiesComponent

--- EstateManagement/ClientApp/app/app.shared.module.ts
+++ EstateManagement/ClientApp/app/app.shared.module.ts
@@ -10,13 +10,21 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 
+//**Properties Section**\\
+import { PropertiesComponent } from './components/properties/properties.component';
+import { PropertiesService } from './components/properties/services/PropertiesService';
+import { PropertiesBackendService } from './services/properties-backend.service';
+import { HttpPropertiesBackendService } from './services/http-properties-backend.service';
+
+
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        PropertiesComponent
     ],
     imports: [
         CommonModule,
@@ -27,9 +35,15 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'properties', component: PropertiesComponent },
             { path: '**', redirectTo: 'home' }
         ])
+    ],
+    providers: [
+        PropertiesService,
+        { provide: PropertiesBackendService, useClass: HttpPropertiesBackendService }
     ]
+
 })
 export class AppModuleShared {
 }
---
+++ EstateManagement/ClientApp/app/components/properties/services/PropertiesService.ts
@@ -0,0 +1,32 @@
+﻿import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import { Property } from '../../../models/Property';
+import { PropertiesBackendService } from '../../../services/properties-backend.service';
+
+@Injectable()
+
+export class PropertiesService {
+    constructor(private propertiesBackenService: PropertiesBackendService) { }
+
+    addProperty(newProperty: Property): Observable<number> {
+        return this.propertiesBackenService.addproperty(newProperty);
+    }
+
+    getProperty(propertyId: number): Observable<Property> {
+        return this.propertiesBackenService.getProperty(propertyId);
+    }
+
+    getAllProperties(): Observable<Property[]> {
+        return this.propertiesBackenService.getProperties();
+    }
+
+    updateProperty(updateProperty: Property): Observable<number> {
+        return this.propertiesBackenService.updateProperty(updateProperty);
+    }
+
+    deleteProperty(propertyId: number): Observable<number> {
+        return this.propertiesBackenService.deleteProperty(propertyId);
+    }
+}
